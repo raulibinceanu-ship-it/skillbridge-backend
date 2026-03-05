f452d7dbd73d4145a0c668a859ed45caedbe6bb4
raulibinceanu-ship-it <raulibinceanu@gmail.com>
2026-03-05 14:20:32 +0100

    Add JWT authentication and login response DTO

--- src/main/java/com/skillbridge/controller/AuthController.java
+++ src/main/java/com/skillbridge/controller/AuthController.java
@@ -1,13 +1,15 @@
 package com.skillbridge.controller;
+
+import com.skillbridge.dto.LoginResponse;
 import com.skillbridge.model.User;
 import com.skillbridge.security.JwtUtil;
 import com.skillbridge.service.UserService;
 import org.springframework.web.bind.annotation.*;
-import com.skillbridge.dto.LoginRequest;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+
     private final UserService userService;
 
     public AuthController(UserService userService) {
@@ -18,14 +20,17 @@ public class AuthController {
     public User register(@RequestBody User user) {
         return userService.registerUser(user);
     }
+
     @PostMapping("/login")
-    public String login(@RequestBody LoginRequest request) {
+    public LoginResponse login(@RequestBody User request) {
 
         User user = userService.loginUser(
                 request.getEmail(),
                 request.getPassword()
         );
 
-        return JwtUtil.generateToken(user.getEmail());
+        String token = JwtUtil.generateToken(user.getEmail());
+
+        return new LoginResponse(token);
     }
 }
